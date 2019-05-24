a4bea44e7f2008b5a341a3be71d269c23e48baa8
PaulienBuskens <32159110+PaulienBuskens@users.noreply.github.com>
2019-05-24 12:07:15 +0200

    database tables

---
+++ database/migrations/2019_05_24_093642_create_practices_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePracticesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('practices', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->string('practice_title');
+            $table->text('practice_body');
+            $table->integer('category_id');
+            $table->string('practice_image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('practices');
+    }
+}
