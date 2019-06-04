2b27b1567d43aa28ca5491bd6d3d991a925f82b0
PaulienBuskens <32159110+PaulienBuskens@users.noreply.github.com>
2019-06-04 18:10:33 +0200

    homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,52 +2,20 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-       <div class="col-md-12 col-md-offset-2">
-            <div class="text-center">
-                <h1>Start your life</h1>
-                <div class="intro">
-                    <p>Bij start your life vind je alles over het zelfstandig gaan wonen. 
-                    <br> Je kan per onderwerp informatie verkrijgen of ervaringen delen met andere.
-                    <br> Verder kan je locatie gebasseerde informatie verkrijgen over bv de winkels in je buurt.
-                    <br>Ten slotte heb je een handige checklist zodat je niets kan vergeten.</p>
-                </div>
-                <div class="row">
-                    <div class="col-md-3">
-                        <a href="/onderwerpen">
-                            <div class="option">
-                                <img src="{{ asset('images/home/onderdelen.png')}}" alt="">
-                                <p class="">Informatie per onderwerp</p>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-md-3">
-                        <a href="/practicesoverview">
-                            <div class="option2">
-                                <img src="{{ asset('images/home/ervaringen.png')}}" alt="">
-                                <p class="">Deel ervaringen</p>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-md-3">
-                        <a href="/practicesoverview">
-                            <div class="option">
-                                <img src="{{ asset('images/home/location.png')}}" alt="">
-                                <p class="">Locatie gebasseerde informatie</p>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-md-3">
-                        <a href="/checklist">
-                            <div class="option2">
-                                <img src="{{ asset('images/home/checklist.png')}}" alt="">
-                                <p class="">Checklist</p>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-       </div>
-    </div>
+   <h1>Start your life</h1>
+   <div class="left__home">
+      <h2>Life starts here</h2>
+      <p>Vraag raad aan groodmoeder of stel je vraag aan de community.</p>
+      <a href='/login'>
+         <div class="rndbtn__home">
+            <p>Wordt deel van de community</p> 
+         </div> 
+      </a>
+   </div>
+   <div class="right__home">
+      <img src="{{ asset('images/rebrand/grandmother.png')}}" alt="ask grandmother">
+      <input type="text" name="search" id="search">
+      <a href="#">VRAAG</a>
+   </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,89 +7,20 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Start Your Life</title>
     <link href="{{ asset('css/index.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    Start Your Life
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item"><a href="{{url('/onderwerpen')}}" class="nav-link">Onderwerpen</a></li>
-                        <li class="nav-item"><a href="{{url('/practicesoverview')}}" class="nav-link">Best Practices</a></li>
-                        <li class="nav-item"><a href="{{url('/practicesoverview')}}" class="nav-link">Locatie gebasseerd</a></li>
-                        <li class="nav-item"><a href="{{url('/checklist')}}" class="nav-link">Checklist</a></li>
-                      <!--  <li class="nav-item"><a href="{{url('/home')}}" class="nav-link">Home</a></li> -->
-                    @guest 
-
-                    @else
-                        
-                        <li class="nav-item"><a href="{{url('/practice')}}" class="nav-link">Add Best Practice</a></li>
-                    @endguest
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            
-                            <li class="nav-item dropdown">
-                            
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ url('/profile') }}">Profile</a>
-                                    <a class="dropdown-item" href="{{ url('/category') }}">Category</a>
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
+    <div>
+        <nav>
+            
         </nav>
 
         <main class="py-4">
             @yield('content')
         </main>
     </div>
+
+    <footer></footer>
 </body>
 </html>
